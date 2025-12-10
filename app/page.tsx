ed831dd3c4e0a57723513b6cae047c2c0e876efa
yuanshikai524 <gcobc04992@gmail.com>
2025-12-10 16:32:53 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,47 @@
 import Image from "next/image";
+import LOGO_IMAGE from "../assets/images/logo.png";
+import SEARCH_ICON from "../assets/icons/search.png";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col w-full h-full">
+      {/* header */}
+      <div className="bg-[#141414C2] flex">
+        {/* 左大邊 */}
+        <div className="flex-1 flex items-center">
+          <Image
+            src={LOGO_IMAGE}
+            alt="logo"
+            loading="eager"
+            className="ml-14 mr-28"
+          />
+          <div className="flex gap-x-10">
+            <p className="text-white text-nowrap">最新上架</p>
+            <p className="text-white text-nowrap">人氣排行</p>
+            <p className="text-white text-nowrap">熱門活動</p>
+            <p className="text-white text-nowrap">重要公告</p>
+            <p className="text-white text-nowrap">網頁儲值</p>
+            <p className="text-white text-nowrap">我的遊戲</p>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        {/* 右邊區塊 */}
+        <div className="flex items-center">
+          <Image src={SEARCH_ICON} alt="search icon" className="w-6 h-6 mr-2" />
+          {/* 登入/註冊 */}
+          <div className="py-4 px-2 flex items-center gap-x-2 mr-4">
+            <button>登入</button>
+            <div className="w-4 h-0 border-b border-[#B0B0B0] rotate-270" />
+            <button>註冊</button>
+          </div>
+          {/* rotate 斜線 */}
+          <div className="w-[60.42px] h-0 border-b border-[#B0B0B0] rotate-[294.44deg]" />
+          {/* 更多 Menu icon */}
+          {/* <Image src={} /> */}
         </div>
-      </main>
+      </div>
+      {/* 大圖 */}
+
+      {/* 遊戲的部分（簡介描述區、輪播圖區） */}
     </div>
   );
 }
